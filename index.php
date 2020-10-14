7825e4901a1818f3b4e0d2b34b3d1164be69109d
Naveed <mnaveed.dev@hotmail.com>
2020-10-15 04:56:06 +0500

    Update index.php

--- index.php
+++ index.php
@@ -28,9 +28,9 @@ var timer;
 
 initDate();
 
-document.getElementById('startButton').addEventListener("click", function() {
-  document.getElementById('value').innerHTML = document.getElementById("myDate").value;
-  var splitValues = document.getElementById("myDate").value.split('-');
+$( document ).ready(function() {
+  document.getElementById('value').innerHTML = '2020-10-21';
+  var splitValues = ["2020","10","21"];
   var end = new Date(splitValues[0], splitValues[1] - 1, splitValues[2]);
   clearInterval(timer);
   showRemaining(end);
