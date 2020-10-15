83aa1d5d9c7e819dc09ba2ea1b8441ece1df1d3b
Naveed <mnaveed.dev@hotmail.com>
2020-10-15 05:02:03 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,29 @@
 <head>
 <title>Birthday Girl</title>
 <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
 </head>
 
 <body>
 
 
-
+<!--
 <input id="myDate" type="date">
 <button id="startButton">Start Countdown</button>
-<div id="value"></div>
-<div id='countdown'></div>
+-->
+
+<div class="container">
+<div class="row">
+
+  <div id="value"></div>
+  <div id='countdown'></div>
+
+</div>
+
+
+</div>
+
 
 
 
